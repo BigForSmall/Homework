b9fe28914e200fe2d8e84c5fc7b3e4b075d27bac
BigForSmall <157493415+BigForSmall@users.noreply.github.com>
2024-02-06 21:53:31 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,19 @@
 function upDate(previewPic){
-    document.getElementById('image').style.backgroundImage = "url("+previewPic.src+")";
-    document.getElementById('image').innerHTML = previewPic.alt;
+  document.getElementById('image').style.backgroundImage = "url("+previewPic.src+")";
+  document.getElementById('image').innerHTML = previewPic.alt;
+  
   }
 
-  function unDo(){
-    document.getElementById('image').style.backgroundImage = "url('')";
-    document.getElementById('image').innerHTML = "Hover over an image below to display here."
+function unDo(){
+  document.getElementById('image').style.backgroundImage = "url('')";
+  document.getElementById('image').innerHTML = "Hover over an image below to display here."
+   
+
   }
+function addFocus(){
+  imgs = document.querySelectorAll(".preview");
+  for(let i = 0; i < imgs.length; i++){
+    imgs[i].setAttribute("tabindex", "0");
+    console.log(imgs[i].src);
+  }
+}
